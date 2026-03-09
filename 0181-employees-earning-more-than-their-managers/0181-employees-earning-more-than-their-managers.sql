9ab8948333c52fd3e7227cb28f0412498d794ed0
Hyun Taek Ki <khjo94@naver.com>
2026-03-09 23:21:05 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT a.name as Employee
 FROM Employee a
-LEFT JOIN Employee b on a.managerId = b.id
+JOIN Employee b on a.managerId = b.id
 WHERE a.salary > b.salary
 AND a.managerId is not null
